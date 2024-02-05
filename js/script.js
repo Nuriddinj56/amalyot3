6ea5daf6fb934fd44a745ebfc1a40c72e77b2175
nuriddinj56 <remboxussi@gmail.com>
2024-02-05 14:20:50 +0500

    card class

--- js/script.js
+++ js/script.js
@@ -139,3 +139,68 @@ document.addEventListener('keydown', (e) => {
     }
 })
 
+// Class
+class MenuCard {
+    constructor(src, alt, title, desor, price, perentSelector) {
+        this.src = src
+        this.alt = alt
+        this.title = title
+        this.desor = desor
+        this.price = price
+        this.parent = document.querySelector(perentSelector)
+        this.transfer = 12400
+        this.chageToUZS()
+    }
+    chageToUZS() {
+        this.price = this.price = this.transfer
+    }
+
+    render() {
+        const element = document.createElement('div')
+
+        element.innerHTML = `
+        
+        <div class="menu__item">
+        <img src=${this.src} alt=${this.alt} />
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">
+         ${this.desor}
+        </div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Price:</div>
+          <div class="menu__item-total"><span>${this.price}</span>uzs/ month</div>
+        </div>
+      </div>
+        
+        `
+        this.parent.append(element)
+    }
+}
+new MenuCard(
+    'img/tabs/2.jp',
+    "elite",
+    'Plan “Premium”',
+    '           Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquid molestiae, sit eveniet, temporaipsum quaerat recusandae sapiente doloremque corporis dolores quas consectetur ut labore distinctio libero reiciendis harum sequi?',
+    "10",
+    '.menu .container'
+).render()
+
+new MenuCard(
+    'img/tabs/2.jp',
+    "elite",
+    'Plan “Premium”',
+    '           Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquid molestiae, sit eveniet, temporaipsum quaerat recusandae sapiente doloremque corporis dolores quas consectetur ut labore distinctio libero reiciendis harum sequi?',
+    "10",
+    '.menu .container'
+).render()
+
+
+new MenuCard(
+    'img/tabs/2.jp',
+    "elite",
+    'Plan “Premium”',
+    '           Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquid molestiae, sit eveniet, temporaipsum quaerat recusandae sapiente doloremque corporis dolores quas consectetur ut labore distinctio libero reiciendis harum sequi?',
+    "10",
+    '.menu .container'
+).render()
